4e7e03f211d28375160160b3af9bab760a903564
printwinter <125651589+printwinter@users.noreply.github.com>
2023-03-13 21:14:24 +0800

    Update two.py

--- two.py
+++ two.py
@@ -1,7 +1,4 @@
-print ("hello")
-print ("这是个非在线论坛")
-def kg():
-  print ("    ")
-print ("https://github.com/printwinter/http/tree/main")
-print ("打开上面链接以获取更新")
-print ("联系qse110@outlook.com以反馈或申请新建话题")
+通过百度网盘分享的文件：setup ws…等3个文件
+链接:https://pan.baidu.com/s/1pRb479iSsmclU15z9YfkCA?pwd=6jqn 
+提取码:6jqn
+复制这段内容打开「百度网盘APP 即可获取」
